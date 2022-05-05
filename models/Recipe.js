de5374677f57d703b7cb89bbaf9b4fdda264ac37
eshkolnik <eshkolnik1@gmail.com>
2022-05-04 22:52:33 -0400

    Update Recipe.js

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,10 +1,9 @@
 'use strict';
 const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
-const ObjectId = Schema.Types.ObjectId;
 const Mixed = Schema.Types.Mixed;
 
-var courseSchema = Schema( {
+var recipeSchema = Schema( {
     name: String,
     ingredients: Mixed,
     steps: Mixed,
